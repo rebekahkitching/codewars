182ced5c35f4f15d2b37aa43ec158dfe1df51b64
Rebekah Kitching <101137372+rebekahkitching@users.noreply.github.com>
2022-06-02 17:28:51 -0700

    Delete 8kyu_beginner_clock directory

--- 8kyu_beginner_clock/main.js
+++
@@ -1,7 +0,0 @@
-function past(h, m, s){
-    let secToMillisec = s * 1000;
-    let minToMillisec = m * 60000;
-    let hoursToMillisec = h * 3600000;
-    
-    return secToMillisec + minToMillisec + hoursToMillisec;
-  }
