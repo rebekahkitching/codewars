b2a564e5789161789a82d85057bfce5f428d7498
Rebekah Kitching <101137372+rebekahkitching@users.noreply.github.com>
2022-06-02 17:33:09 -0700

    Delete 8kyu_needle_haystack directory

--- 8kyu_needle_haystack/main.js
+++
@@ -1,9 +0,0 @@
-function findNeedle(haystack) {
-    for(index = 0;index < haystack.length; index++){
-      let element = haystack[index];
-      
-      if(String(element) === 'needle'){
-        return 'found the needle at position ' + haystack.indexOf(element);
-      }
-    }
-  }
