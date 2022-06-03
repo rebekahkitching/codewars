9308e882c2a4f782ba916ca49827458114ae72f8
Rebekah Kitching <101137372+rebekahkitching@users.noreply.github.com>
2022-06-02 17:32:44 -0700

    Delete 8kyu_mean_of_array directory

--- 8kyu_mean_of_array/main.js
+++
@@ -1,9 +0,0 @@
-const getAverage = (marks) => {
-    const total = marks.reduce((acc,c) => acc + c);
-    return Math.floor(total / marks.length);
-  }
-  
-  
-  // reduce list of nums into single num via addition
-  // divide the sum by num of items in list for average
-  // take .floor(), creating floor average 
